b1ebf76f6d12d4d96834c874b6f85f3ee9d3e8d3
Gamerman2001 <Flash@heysparkbox.com>
2021-02-25 13:25:16 -0500

    test: Backstage & Sulfuras test - activated Sulfuras test on line 100 - refactored Backstage Pass tests to work on new results array from function updateQuantity located in gilded_rose.js

--- src/gilded_rose.spec.js
+++ src/gilded_rose.spec.js
@@ -67,31 +67,37 @@ describe('updating of backstage passes', () => {
   });
 
   it('decreases the sell_in of backstage passes by 1', () => {
-    expect(pass.sell_in).toBe(10);
+    const backstagePassArray = updateQuality([pass]);
+    expect(backstagePassArray[0].sell_in).toBe(10);
   });
 
   it('increases the quality of backstage passes by 2 if there are 10 sell_in days or less', () => {
-    expect(passTenDays.quality).toBe(10);
+    const backstagePassArray = updateQuality([passTenDays]);
+    expect(backstagePassArray[0].quality).toBe(10);
   });
 
   it('increases the quality of backstage passes by 3 if there are 5 sell_in days or less', () => {
-    expect(passFiveDays.quality).toBe(18);
+    const backstagePassArray = updateQuality([passFiveDays]);
+    expect(backstagePassArray[0].quality).toBe(18);
   });
 
   it('decreases the quality of backstage passes to 0 if there are 0 sell_in days or less', () => {
-    expect(passPostConcert.quality).toBe(0);
+    const backstagePassArray = updateQuality([passPostConcert]);
+    expect(backstagePassArray[0].quality).toBe(0);
   });
 
   it('does not update the quality of backstage passes to more than 50', () => {
-    expect(passMaximumQuality.quality).toBe(50);
+    const backstagePassArray = updateQuality([passMaximumQuality]);
+    expect(backstagePassArray[0].quality).toBe(50);
   });
 
   it('does not reduce the quality of backstage passes to below zero', () => {
-    expect(passMinimumQuality.quality).toBe(0);
+    const backstagePassArray = updateQuality([passMinimumQuality]);
+    expect(backstagePassArray[0].quality).toBe(0);
   });
 });
 
-describe.only('updating of sulfuras', () => {
+describe('updating of sulfuras', () => {
   const sulfuras = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
 
   it('does not update sell_in', () => {
