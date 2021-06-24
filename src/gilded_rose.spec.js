619d0a1d96e1fdc874104cbcf9af0f3ec934e751
Jordan Leven <jordan@heysparkbox.com>
2021-06-24 15:56:19 -0400

    FIXUP

--- src/gilded_rose.spec.js
+++ src/gilded_rose.spec.js
@@ -1,10 +1,10 @@
 import { Item, updateQuality } from './gilded_rose';
 
 describe('`updateQuality`', () => {
-  it.todo("Why won't my test pass?", () => {
+  it('deprecates the sell in by one for a Haunted Shoe', () => {
     const standardItem = new Item('Haunted Shoe', 10, 10);
     updateQuality([standardItem]);
-    expect(standardItem.sell_in).toBe(4);
+    expect(standardItem.sell_in).toBe(9);
   });
 
   it.todo('This is a good place for a good test!');
