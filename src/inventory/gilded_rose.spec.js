1380e8d626396f444a9c022c18097f74b03755d9
Angus Chang <angus@heysparkbox.com>
2021-07-21 16:41:58 -0400

    test: added tests for two more helper functions for conjure items and backstage passes.

--- src/inventory/gilded_rose.spec.js
+++ src/inventory/gilded_rose.spec.js
@@ -7,11 +7,43 @@ describe('Helper functions', () => {
     expect(sellInDecreasesItem(increasingItem)).toBe(false);
     expect(sellInDecreasesItem(decreasingItem)).toBe(true);
   });
-  it('increases quality of Aged Brie', () => {
+  it('handles conditions of Aged Brie', () => {
     const agedBrie = new Item('Aged Brie', 2, 0);
     handleBrie(agedBrie);
+    // increases quality as sell in decreases;
     expect(agedBrie.quality).toBe(1);
     expect(agedBrie.sell_in).toBe(1);
+  });
+  it('handles conditions of Sulfuras', () => {
+    const sulfuras = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
+  });
+  it('handles conditions of Backstage Passes', () => {
+    const aboveTenDaysBackstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', 11, 20);
+    const tenDaysBackstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20);
+    const fiveDaysBackstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', 5, 20);
+    const noDaysBackstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', -1, 20);
+    handleBackstagePass(aboveTenDaysBackstagePass);
+    handleBackstagePass(tenDaysBackstagePass);
+    handleBackstagePass(fiveDaysBackstagePass);
+    handleBackstagePass(noDaysBackstagePass);
+    // increases by one with age;
+    expect(tenDaysBackstagePass.quality).toBe(21);
+    expect(tenDaysBackstagePass.sell_in).toBe(10);
+    // increases by 2 within 10 days of show;
+    expect(tenDaysBackstagePass.quality).toBe(22);
+    expect(tenDaysBackstagePass.sell_in).toBe(9);
+    // increase by 3 within 5 days of show;
+    expect(fiveDaysBackstagePass.quality).toBe(23);
+    expect(fiveDaysBackstagePass.sell_in).toBe(4);
+    // loses all value after show
+    expect(noDaysBackstagePass.quality).toBe(0);
+    expect(noDaysBackstagePass.sell_in).toBe(-2);
+  });
+  it('handles conditions of Conjured items', () => {
+    const conjuredItem = new Item('Conjured Mana Cake', 3, 6);
+    handleConjuredItem(conjuredItem);
+    expect(conjuredItem.quality).toBe(4);
+    expect(conjuredItem.sell_in).toBe(2);
   });
 });
 
